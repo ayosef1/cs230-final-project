f2d0d36ae78aae5bc3b283c1bef40be1ea105494
Abraham Yosef <abrahamyosef@DN51v54f.SUNet>
2024-12-02 19:23:38 -0800

    test best model (trial 14)

--- test/lstm_nn_test.py
+++ test/lstm_nn_test.py
@@ -9,9 +9,28 @@ def main():
     raw_data = 'data/imdb_dataset.csv'
     processed_data_file = 'data/imdb_dataset.pkl'
 
-    model = LSTMClassifier(raw_data, processed_data_file)
+    embedding_dim = 100
+    lstm_units = 64
+    dropout_rate = 0.4
+    dense_units = 96
+    batch_size = 128
+    learning_rate = 0.0001681035160630061
 
+    model = LSTMClassifier(
+            raw_data=raw_data,
+            processed_data_file=processed_data_file,
+            embedding_dim=embedding_dim,
+            lstm_units=lstm_units,
+            dropout_rate=dropout_rate,
+            dense_units=dense_units,
+            batch_size=batch_size,
+            epochs=10,
+            learning_rate=learning_rate,
+            checkpoint='trial14.h5'
+        )
+    
     model.train()
+    print("TRAINING COMPLETE\n\n\n")
     model.test()
     
 if __name__ == "__main__":
